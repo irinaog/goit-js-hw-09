6b3b060d653998dfb2889b9a6973d4c98fa2bd4e
irinaog <goncharirina08@gmail.com>
2022-05-12 11:24:09 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+console.dir(btnStart);
+
+btnStart.addEventListener('click', () => {changeBcgColor()});
+btnStop.addEventListener('click', () => { stopChangeBcgColor()});
+let intervalId;
+
+function changeBcgColor() {
+    intervalId = setInterval(() => {
+        colorSwitcher(getRandomHexColor());
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+    }, 1000);   
+    
+};
+
+function stopChangeBcgColor() {
+    clearInterval(intervalId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+function colorSwitcher(color) {
+    const bcgBody = document.querySelector('body');
+    bcgBody.style.backgroundColor = `${color}`;  
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+
 
